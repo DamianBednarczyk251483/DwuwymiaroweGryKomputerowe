a719ddd0ebf662eecd52ab0981bf70c36b9950d8
DamianBednarczyk251483 <251483@edu.p.lodz.pl>
2025-10-04 13:03:47 +0200

    Zadanie 1 - drobne poprawki (wersja do pierwszego przesłania)

--- Gra2D/Gra2D.cpp
+++ Gra2D/Gra2D.cpp
@@ -7,13 +7,16 @@ std::vector<sf::Sprite> sprites;
 
 void moveSprite(sf::Sprite& sprite, float offsetX, float offsetY)
 {
-    sprite.setPosition(sprite.getPosition().x + offsetX, sprite.getPosition().y + offsetY);
+	sf::Vector2f position = sprite.getPosition();
+    sprite.setPosition(position.x + offsetX, position.y + offsetY);
 }
 
 sf::Sprite stringToSprite(std::string file)
 {
-    if (!textures[file].loadFromFile("Assets/" + file + ".png"))
-		throw std::runtime_error("Failed to load texture: " + file);
+    if (textures.find(file) == textures.end()) {
+        if (!textures[file].loadFromFile("Assets/" + file + ".png"))
+            throw std::runtime_error("Failed to load texture: " + file);
+    }
 
     sf::Sprite sprite(textures[file]);
     sprite.setScale(2, 2);
@@ -33,21 +36,24 @@ int main()
 
 
     sprites.push_back(stringToSprite("polana_360x180"));
-    sf::Sprite ludzik = stringToSprite("ludzik");
-	ludzik.setPosition(-100, 240);
-    sprites.push_back(ludzik);
+    sprites.push_back(stringToSprite("ludzik"));
+    sprites[1].setPosition(-100, 240);
+
+	sf::Clock clock;
+	float deltaTime;
+	float speed = 100.0f;
 
     while (window.isOpen())
     {
+		deltaTime = clock.restart().asSeconds();
         sf::Event event;
         while (window.pollEvent(event))
         {
             if (event.type == sf::Event::Closed)
                 window.close();
         }
-
         window.clear(sf::Color(164,221,219));
-		moveSprite(sprites[1], 0.03f, 0);
+		moveSprite(sprites[1], speed * deltaTime, 0);
         drawAll(window);
         window.display();
     }
